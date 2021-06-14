48acecd68ef5824ef52a71d8f3315837a7959d15
AlexDespod <kolesniks559@gmail.com>
2021-06-14 09:58:11 +0300

    second

--- tests/Unit/FirstTest.php
+++ tests/Unit/FirstTest.php
@@ -3,19 +3,39 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-use Src\General;
+use Src\Fly;
 
 class FirstTest extends TestCase
 {
-    private $Gen;
+    private $Fly;
 
     protected function setUp():void
     {
-        $this->Gen = new General();
+        $this->Fly = new Fly();
     }
 
-    public function testFirst():void
+    public function testGetName():void
     {
-        $this->assertSame(22,$this->Gen->firstFunc(1));
+        $name = $this->Fly->getNameById("E",1);
+        $this->assertSame("Kolesnyk",$name);
+    }
+
+
+
+    /**
+     * @dataProvider classesProvider
+     */
+    public function testCountOfPlaces(string $var,int $expected)
+    {
+        $count = $this->Fly->countOfTicketsByClass($var);
+        $this->assertSame($expected,$count);
+    }
+    public function classesProvider()
+    {
+        return [
+            ["E",3],
+            ["B",2],
+            ["S",4],
+        ];
     }
 }
